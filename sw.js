69c865c2585045ba7f90dbfe0de3e4619c4fe68e
Amrithnath <amrith@Tydy.it>
2025-03-14 00:49:28 +0530

    :bug: fixes service worker

--- sw.js
+++ sw.js
@@ -8,3 +8,14 @@ while (lengthOfTree > state.pageLength) {
 }
 
 
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+});
