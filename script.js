150c720fc79510090c056b65678b4536cf4d9527
Amrithnath <amrith@Tydy.it>
2025-03-14 00:46:25 +0530

    :bug: fixes service worker

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ async function loadPost(id){
 const registerServiceWorker = async () => {
 if ("serviceWorker" in navigator) {
     try {
-    const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+    const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/index.html" });
     if (registration.installing) {
         console.log("Service worker installing");
     } else if (registration.waiting) {
